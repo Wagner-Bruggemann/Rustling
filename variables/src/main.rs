c8c7596f45827f849cb025e4337cab788afd9b02
Wagner Brüggemann <wagner.bruggemann@gmail.com>
2026-02-10 21:13:33 -0300

    [chapter-three] feat: Mutations and Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,17 @@
+const CONSTANT: i32 = 1;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
 
     x = 6;
     println!("The value of x is: {x}");
+
+    println!("The value of CONSTANT is: {CONSTANT}");
+
+    let shadow = "Value was not shadowed";
+    println!("{shadow}");
+
+    let shadow = "Value was shadowed";
+    println!("{shadow}");
 }
